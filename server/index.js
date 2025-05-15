1dee8126e090a623c2e58821dfa8d74ffc07dae7
mrWalter1 <mozg.ghg@gmail.com>
2025-05-15 20:54:29 +0300

    обновил код

---
+++ server/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.listen(PORT, () =>
+  console.log(`Сервер запущен на порте ${PORT}`)
+);
