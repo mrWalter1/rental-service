003fede495134986b20eca218b6df8d29bff7433
mrWalter1 <mozg.ghg@gmail.com>
2025-05-15 21:17:47 +0300

    первый маршрут

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import sequelize from './config/database.js';
+import cors from 'cors';
+import './models/user.js';
+import './models/offer.js';
+import './models/review.js';
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.listen(PORT, () =>
-  console.log(`Сервер запущен на порте ${PORT}`)
-);
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () =>
+      console.log(`Сервер и БД запущены на порте ${PORT}`)
+    );
+  } catch (e) {
+    console.error('Ошибка запуска сервера:', e);
+  }
+};
+start();
+
+app.use(cors());
+app.use(express.json());
