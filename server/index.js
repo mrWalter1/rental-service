25431c911cbfc9d09ced7be926b42abc9c882e87
mrWalter1 <mozg.ghg@gmail.com>
2025-05-22 01:02:39 +0300

    Раздача статических файлов

--- server/index.js
+++ server/index.js
@@ -4,14 +4,20 @@ import sequelize from './config/database.js';
 import cors from 'cors';
 import router from './routes/index.js';
 import ErrorHandlingMiddleware from './middleware/ErrorHandlingMiddleware.js';
+import path        from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Ура! Всё заработало!' });
