c7f148de0c66bea1ca038ac1020807c8318c3ed9
mrWalter1 <mozg.ghg@gmail.com>
2025-05-28 18:43:56 +0300

    создал authMiddleware.js

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+// server/middleware/authMiddleware.js
+
+import jwt from 'jsonwebtoken';
+import ApiError from '../error/ApiError.js';
+import { User } from '../models/user.js';
+
+export const authenticateToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return next(ApiError.unauthorized('Нет токена'));
+    }
+    const token = authHeader.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findByPk(decoded.id);
+    if (!user) {
+      return next(ApiError.unauthorized('Пользователь не найден'));
+    }
+    req.user = user;
+    next();
+  } catch {
+    return next(ApiError.unauthorized('Недействительный токен'));
+  }
+};
