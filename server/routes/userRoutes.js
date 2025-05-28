8d4fafca602dc0f2612fe4d3d1bf0a0c318fd5f9
mrWalter1 <mozg.ghg@gmail.com>
2025-05-28 18:58:22 +0300

    обновил маршрут

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@
 import { Router }            from 'express';
 import upload                from '../middleware/upload.js';
 import { registration }      from '../controllers/userController.js';
+import { registration, login, checkAuth, logout } from '../controllers/userController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
 
 const router = new Router();
