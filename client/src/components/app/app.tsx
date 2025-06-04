9506d3fbf796b48abb7e95844ca4a45608e937ce
mrWalter1 <mozg.ghg@gmail.com>
2025-06-04 23:48:33 +0300

    обновил app.tsx

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,41 +1,53 @@
 // src/components/app/app.tsx
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MainPage}      from '../../pages/main-page/main-page';
-import { LoginPage }    from '../../pages/login-page/login-page';
-import { FavoritesPage }from '../../pages/favorites-page/favorites-page';
-import { OfferPage  }   from '../../pages/offer-page/offer-page';
-import { EmptyPage }    from '../../pages/empty-page/empty-page';
-import { AppRoute }  from '../../const';
 import React, { JSX } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import {MainPage} from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';        // <-- default import
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import {EmptyPage} from '../../pages/empty-page/empty-page';
+
 import { PrivateRoute } from '../private-route/private-route';
-import { AuthorizationStatus } from '../../const';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { FullOffer } from '../../types/offer';
 
 type AppMainPageProps = {
   rentalOffersCount: number;
+  offers:            FullOffer[];
 };
 
-function App({ rentalOffersCount }: AppMainPageProps): JSX.Element {
+function App({ rentalOffersCount, offers }: AppMainPageProps): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path={AppRoute.Main}
-          element={<MainPage rentalOffersCount={rentalOffersCount} />}
+          element={
+            <MainPage
+              rentalOffersCount={rentalOffersCount}
+              offers={offers}
+            />
+          }
         />
-        <Route path={AppRoute.Login} element={<LoginPage />} />
+
+        <Route path={AppRoute.Login} element={<LoginPage />} />      {/* <-- default import */}
 
         <Route
           path={AppRoute.Favorites}
           element={
             <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
-              {/* TODO: вместо NO_AUTH передавать реальный статус из состояния */}
-              <FavoritesPage />
+              <FavoritesPage offers={offers} />
             </PrivateRoute>
           }
         />
 
-        <Route path={AppRoute.Offer} element={<OfferPage />} />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage offers={offers} />}
+        />
+
         <Route path="*" element={<EmptyPage />} />
       </Routes>
     </BrowserRouter>
@@ -43,3 +55,4 @@ function App({ rentalOffersCount }: AppMainPageProps): JSX.Element {
 }
 
 export default App;
+
