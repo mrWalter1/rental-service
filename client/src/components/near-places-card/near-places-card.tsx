976c44781363c2c62a4e8c04344d1911095d62e4
mrWalter1 <mozg.ghg@gmail.com>
2025-06-05 21:01:22 +0300

    обновление списка предложений рядом

--- client/src/components/near-places-card/near-places-card.tsx
+++ client/src/components/near-places-card/near-places-card.tsx
@@ -1,49 +1,61 @@
 // src/components/near-places-card/near-places-card.tsx
-
 import React, { JSX } from 'react';
+import { Link } from 'react-router-dom';
+import { FullOffer } from '../../types/offer';
 
-function NearPlacesCard(): JSX.Element {
+type NearPlacesCardProps = {
+  offer: FullOffer;
+};
+
+function NearPlacesCard({ offer }: NearPlacesCardProps): JSX.Element {
   return (
     <article className="near-places__card place-card">
+      {offer.isPremium && (
+        <div className="place-card__mark">
+          <span>Premium</span>
+        </div>
+      )}
       <div className="near-places__image-wrapper place-card__image-wrapper">
-        <a href="#">
+        <Link to={`/offer/${offer.id}`}>
           <img
             className="place-card__image"
-            src="/img/room.jpg"
+            src={`/img/${offer.previewImage}`}
             width="260"
             height="200"
             alt="Place image"
           />
-        </a>
+        </Link>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;80</b>
+            <b className="place-card__price-value">&euro;{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button
-            className="place-card__bookmark-button place-card__bookmark-button--active button"
+            className={`place-card__bookmark-button ${
+              offer.isFavorite ? 'place-card__bookmark-button--active' : ''
+            } button`}
             type="button"
           >
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use href="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">In bookmarks</span>
+            <span className="visually-hidden">
+              {offer.isFavorite ? 'In bookmarks' : 'To bookmarks'}
+            </span>
           </button>
         </div>
-
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
+            <span style={{ width: `${offer.rating * 20}%` }}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
-
         <h2 className="place-card__name">
-          <a href="#">Wood and stone place</a>
+          <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
         </h2>
-        <p className="place-card__type">Private room</p>
+        <p className="place-card__type">{offer.type}</p>
       </div>
     </article>
   );
